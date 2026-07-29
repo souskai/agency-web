46dbfc975c6a99352fd802daf94cf177adf366f2
fontius <fontius@gmail.com>
2026-07-29 09:36:10 -0400

    fix: remove invert class from logo for correct dark navbar color (#13)
    Removes invert/dark:invert-0 from Logo so it renders white on dark
    navbar and black on light navbar.

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -57,7 +57,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     >
       <div className="container py-4 flex justify-between items-center">
         <Link href={prefixWithLocale('/', locale)}>
-          <Logo loading="eager" priority="high" className="invert dark:invert-0" />
+          <Logo loading="eager" priority="high" />
         </Link>
         <HeaderNav data={data} />
       </div>
